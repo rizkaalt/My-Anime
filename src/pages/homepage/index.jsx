3d45ea0a3143635f9eb876964a634c193bf65fc1
RIZKA    ----- Anime Lovers ----- <116262828+rizkaalt@users.noreply.github.com>
2023-06-19 06:17:29 +0000

    Update index.jsx

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -4,10 +4,10 @@ const Homepage = () => {
 
     return (
         <>
-            {/<h1>hello {watch.description}</h1>
+            {/ <h1>hello {watch.description}</h1>
             <button className="ring-1 ring-blue-600 px-3 py-2" onClick={changeUser}>Ganti User</button>
             <br />
-            <Link to="/details">Details</Link>/}
+            <Link to="/details">Details</Link> /}
         </>
     )
 }
